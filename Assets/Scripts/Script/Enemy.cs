abfec8d9a282873923a3b5f30d51150a3bf02f79
chomyeongjin <141563326+chomyeongjin@users.noreply.github.com>
2024-11-08 22:37:19 +0900

    EnemyShooting

--- Assets/Scripts/Script/Enemy.cs
+++ Assets/Scripts/Script/Enemy.cs
@@ -4,10 +4,18 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+     // 좌우이동 + following player
     public int speed = 2;
     Vector2 startPos;
     bool follow = false;
     public float followSpeed = 2;
+
+    // Shooting
+    public GameObject bulletPref;
+    public bool canShoot = false;
+    float bulletSpeed = 10.0f;
+    float spawnInterval = 0.6f;
+    float nextSpawn = 0f;
     
     void Start()
     {
@@ -23,8 +31,16 @@ public class Enemy : MonoBehaviour
             {
                 Vector2 targetPos = player.transform.position;
                 transform.position = Vector2.MoveTowards(transform.position, targetPos, followSpeed * Time.deltaTime);
+
+                // Shooting
+                if(canShoot && Time.time >= nextSpawn)
+                {
+                    Shooting(player.transform);
+                    nextSpawn = Time.time + spawnInterval;
+                }
             }
-        } else {
+        }
+        else {
             float xPos = Mathf.PingPong(Time.time * speed, 5) - (5 / 2);
             transform.position = Vector2.Lerp(transform.position, new Vector3(startPos.x + xPos, transform.position.y), Time.deltaTime * speed);
         }
@@ -34,6 +50,7 @@ public class Enemy : MonoBehaviour
     {
         if(other.gameObject.name == "Player") {
             follow = true;
+            canShoot = true;
         }
     }
 
@@ -42,7 +59,19 @@ public class Enemy : MonoBehaviour
         if (other.gameObject.name == "Player")
         {
             follow = false;
+            canShoot = false;
             startPos = transform.position;
+        }
+    }
+
+    void Shooting(Transform player)
+    {
+        GameObject bullet = Instantiate(bulletPref, transform.position, Quaternion.identity);
+        Rigidbody2D bulletrb = bullet.GetComponent<Rigidbody2D>();
+
+        if(bulletrb != null) {
+            Vector2 direction = (player.position - transform.position).normalized;
+            bulletrb.velocity = direction * bulletSpeed;
         }
     }
 }
