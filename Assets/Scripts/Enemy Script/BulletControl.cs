943255416685af5b62abf79503a9db16dd9e3918
chomyeongjin <141563326+chomyeongjin@users.noreply.github.com>
2024-12-25 12:12:12 +1100

    애니메이션 스프라이트 전체 수정

--- Assets/Scripts/Enemy Script/BulletControl.cs
+++ Assets/Scripts/Enemy Script/BulletControl.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class BulletControl : MonoBehaviour
 {
+    void Update()
+    {
+        Destroy(gameObject, 4f);
+    }
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.name == "Player") {
