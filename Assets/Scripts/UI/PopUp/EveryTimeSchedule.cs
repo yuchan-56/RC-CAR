c6ee054b72abfc203c36b49b7b636417f8a2d759
rlaeodbsdk <147009763+rlaeodbsdk@users.noreply.github.com>
2025-02-04 16:11:59 +0900

    Feat : Stage 클릭시 노란 테두리 구현

--- Assets/Scripts/UI/PopUp/EveryTimeSchedule.cs
+++ Assets/Scripts/UI/PopUp/EveryTimeSchedule.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 public class EveryTimeSchedule : UI_Popup
 {
+    public GameObject[] obj;
+    
     enum Stage { //Sx_y 에서 x -> 월화수목금.. y는 교시
         S1_1,
         S1_2,
@@ -11,6 +14,15 @@ public class EveryTimeSchedule : UI_Popup
         S1_4
 
     };
+    enum Line
+    {
+        S1_1_Line,
+        S1_2_Line,
+        S1_3_Line,
+        S1_4_Line,
+        S1_5_Line,
+
+    }
 
     private void Start()
     {
@@ -24,10 +36,24 @@ public class EveryTimeSchedule : UI_Popup
         base.Init();
         SetResolution();
         Bind<Button>(typeof(Stage));
+        Bind<Image>(typeof(Line));
+
         GetButton((int)Stage.S1_1).gameObject.AddUIEvent(StageGo);
         GetButton((int)Stage.S1_2).gameObject.AddUIEvent(StageGo);
         GetButton((int)Stage.S1_3).gameObject.AddUIEvent(StageGo);
         GetButton((int)Stage.S1_4).gameObject.AddUIEvent(StageGo);
+
+
+        obj[0] = GetImage((int)Line.S1_1_Line).gameObject;
+        obj[1] = GetImage((int)Line.S1_2_Line).gameObject;
+        obj[2] = GetImage((int)Line.S1_3_Line).gameObject;
+        obj[3] = GetImage((int)Line.S1_4_Line).gameObject;
+        obj[4] = GetImage((int)Line.S1_5_Line).gameObject;
+        Debug.Log(obj);
+        for(int i =0;i<5;i++)
+        {
+            obj[i].SetActive(false);
+        }
     }
 
     private void StageGo(PointerEventData eventData)    
@@ -45,8 +71,17 @@ public class EveryTimeSchedule : UI_Popup
 
             Managers.UI.ShowPopUpUI<StageInfo>();
             Managers.UI.isPopuping = true;
+
         }
 
+        for (int k = 0; k < 5; k++) // 테두리 초기화
+        {
+            obj[k].SetActive(false);
+        }
+
+        int i = int.Parse(StageName[3].ToString());
+        Debug.Log(i);
+        obj[i-1].SetActive(true);
 
     }
     public void OnBeginDrag(PointerEventData eventData)
