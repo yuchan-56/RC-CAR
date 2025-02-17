f85f26ffede3e4083d4bea4ff2c5588ac19c9e53
choijeanwoo <jwoochoi0210@gmail.com>
2025-02-17 23:49:51 +0900

    ButtonAni

--- Assets/Scripts/Managers/ComboManager.cs
+++ Assets/Scripts/Managers/ComboManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
+using Unity.VisualScripting;
+using UnityEditor;
 
 public class ComboManager : MonoBehaviour
 {
@@ -13,6 +15,12 @@ public class ComboManager : MonoBehaviour
     public PlayerMove player;
     public PlayerAttackGeneral playerAttackGeneral;
     public PlayerAttackAnimation ani;
+    public AtkAni atkAni;
+    public DashAni dashAni;
+    public JumpAni jumpAni;
+    private Coroutine dashAniCoroutine;
+    private Coroutine jumpAniCoroutine;
+    private Coroutine atkAniCoroutine;
     bool AniSetup;
     GameObject CurrentObject;
 
@@ -48,97 +56,108 @@ public class ComboManager : MonoBehaviour
             if (InputButton.Count == 1 && AniSetup == false)
             {
                 AniSetup = true;
-                if(InputButton.Contains("Dash"))
+                if (InputButton.Contains("Dash"))
                 {
-                    
+                    if (dashAni != null)
+                    {
+                        StopExistingCoroutine(ref dashAniCoroutine);
+                        dashAniCoroutine = StartCoroutine(dashAni.AnimateButton());
+                    }
                 }
-
-                else if(InputButton.Contains("Jump"))
+                else if (InputButton.Contains("Jump"))
                 {
-
+                    if (jumpAni != null)
+                    {
+                        StopExistingCoroutine(ref jumpAniCoroutine);
+                        jumpAniCoroutine = StartCoroutine(jumpAni.AnimateButton());
+                    }
                 }
-
-                else if(InputButton.Contains("Attack"))
+                else if (InputButton.Contains("Attack"))
                 {
-
+                    if (atkAni != null)
+                    {
+                        StopExistingCoroutine(ref atkAniCoroutine);
+                        atkAniCoroutine = StartCoroutine(atkAni.AnimateButton());
+                    }
                 }
-
                 else
                 {
                     AniSetup = false;
+                    InputButton.Clear();
                 }
             }
         }
 
-        else if (Input.GetMouseButtonUp(0))
+        // 마우스를 뗐을 때 실행
+        else if (Input.GetMouseButtonUp(0) && AniSetup == true)
         {
+            Debug.Log("HashSet Contents: " + string.Join(", ", InputButton));
+
+            // 실행 중인 코루틴 즉시 종료
+            StopExistingCoroutine(ref atkAniCoroutine);
+            StopExistingCoroutine(ref dashAniCoroutine);
+            StopExistingCoroutine(ref jumpAniCoroutine);
+
+            // 즉시 초기화
+            atkAni.ResetImage();
+            dashAni.ResetImage();
+            jumpAni.ResetImage();
+            atkAni.isAnimating = false;
+            dashAni.isAnimating = false;
+            jumpAni.isAnimating = false;
+
             switch (InputButton.Count)
             {
                 case 1:
+                    if (InputButton.Contains("Jump"))
                     {
-                        if (InputButton.Contains("Jump")) // Jump
-                        {
-                            Debug.Log("Jump");
-                        }
-
-                        else if (InputButton.Contains("Dash")) //Dash
-                        {
-                            Debug.Log("Dash");
-                        }
-
-                        else if (InputButton.Contains("Attack"))
-                        {
-                            Debug.Log("Attack"); //Atack
-                            playerAttackGeneral.AttackSetActive();
-                            player.SkillMotionActive("Attack");
-                        }
-
-                        break;
+                        Debug.Log("Jump");
                     }
+                    else if (InputButton.Contains("Dash"))
+                    {
+                        Debug.Log("Dash");
+                    }
+                    else if (InputButton.Contains("Attack"))
+                    {
+                        Debug.Log("Attack");
+                        playerAttackGeneral.AttackSetActive();
+                        player.SkillMotionActive("Attack");
+                    }
+                    break;
 
                 case 2:
+                    if (InputButton.Contains("Jump") && InputButton.Contains("Dash"))
                     {
-                        if (InputButton.Contains("Jump") && InputButton.Contains("Dash"))//점프대쉬
-                        {
-                            Debug.Log("JumpDash active");
-                            player.jump();
-                            player.TriggerDash();
-                            player.SkillMotionActive("JumpDash");
-
-                        }
-
-                        else if (InputButton.Contains("Dash") && InputButton.Contains("Attack"))//대쉬어택
-                        {
-                            Debug.Log("DashAttack active");
-                            player.TriggerDash();
-                            dashAttack.SkillMotionActive();
-                            player.SkillMotionActive("DashAttack");
-
-                        }
-
-                        else if(InputButton.Contains("Jump") && InputButton.Contains("Attack"))
-                        {
-                            Debug.Log("JumpAttack active");//점프어택
-                            player.jump();
-
-                            jumpAttack.SkillMotionActive();
-                            player.SkillMotionActive("JumpAttack");
-
-                        }
-
-                        break;
+                        Debug.Log("JumpDash active");
+                        player.jump();
+                        player.TriggerDash();
+                        player.SkillMotionActive("JumpDash");
                     }
+                    else if (InputButton.Contains("Dash") && InputButton.Contains("Attack"))
+                    {
+                        Debug.Log("DashAttack active");
+                        player.TriggerDash();
+                        dashAttack.SkillMotionActive();
+                        player.SkillMotionActive("DashAttack");
+                    }
+                    else if (InputButton.Contains("Jump") && InputButton.Contains("Attack"))
+                    {
+                        Debug.Log("JumpAttack active");
+                        player.jump();
+                        jumpAttack.SkillMotionActive();
+                        player.SkillMotionActive("JumpAttack");
+                    }
+                    break;
 
                 case 3:
+                    if (Managers.Game.gage >= 0 && InputButton.Contains("Jump") && InputButton.Contains("Attack") && InputButton.Contains("Dash"))
                     {
-                        if (Managers.Game.gage >= 0 && InputButton.Contains("Jump") && InputButton.Contains("Attack") && InputButton.Contains("Dash"))
-                        {
-                            Debug.Log("Ultimate skill active");
-                            playerAttackGeneral.UltimateSkillActive();
-                        }
-                        break;
+                        Debug.Log("Ultimate skill active");
+                        playerAttackGeneral.UltimateSkillActive();
                     }
+                    break;
             }
+
             AniSetup = false;
             InputButton.Clear();
         }
@@ -149,6 +168,14 @@ public class ComboManager : MonoBehaviour
             Debug.Log(Managers.Game.gage);
             Managers.Game.GetHit = false;
         }
+    }
 
+    private void StopExistingCoroutine(ref Coroutine coroutine)
+    {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+            coroutine = null;
+        }
     }
 }
---
+++ Assets/Scripts/Player/Skill/UI/JumpAni.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class JumpAni : MonoBehaviour
+{
+    public Sprite[] animationSprites; // 애니메이션에 사용할 스프라이트 배열
+    public float frameRate = 0.1f; // 프레임 간격 (초 단위)
+    private Image buttonImage;
+    public bool isAnimating = false;
+
+    void Awake()
+    {
+        buttonImage = GetComponent<Image>();
+    }
+
+    public IEnumerator AnimateButton()
+    {
+        if (isAnimating || animationSprites.Length == 0)
+            yield break;
+
+        isAnimating = true;
+        for (int i = 0; i < animationSprites.Length; i++)
+        {
+            buttonImage.sprite = animationSprites[i];
+            yield return new WaitForSeconds(frameRate);
+        }
+
+        isAnimating = false;
+    }
+    public void ResetImage()
+    {
+        buttonImage.sprite = animationSprites[0];
+    }
+}
