b7d327b8063186de01cc5f9e864c673260267954
darkbaag12 <80233257+darkbaag12@users.noreply.github.com>
2025-02-21 17:19:10 +0900

    플레이어 대쉬 속도 조절

--- Assets/Scripts/taehyun Script/PlayerMove.cs
+++ Assets/Scripts/taehyun Script/PlayerMove.cs
@@ -14,7 +14,7 @@ public class PlayerMove : MonoBehaviour
     float jumpforce = 10f;
     bool isground = false;
     float dashSpeed = 20f;
-    public float dashDuration = 0.1f;
+    public float dashDuration = 1f;
     float dashCoolDown = 0.1f;
     bool isDashing = false;
     bool canDash = true;
