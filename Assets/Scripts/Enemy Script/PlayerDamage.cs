8aadfe25117b1aa46cf3b13cc3ccae29fe6b67d4
choijeanwoo <jwoochoi0210@gmail.com>
2025-02-26 18:29:51 +0900

    playerHP

--- Assets/Scripts/Enemy Script/PlayerDamage.cs
+++ Assets/Scripts/Enemy Script/PlayerDamage.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PlayerDamage : MonoBehaviour
 {
+    public PlayerHP playerHP;
     // Start is called before the first frame update
     void Start()
     {
@@ -20,7 +21,7 @@ public class PlayerDamage : MonoBehaviour
     {
         if(other.CompareTag("Player")) {
             Debug.Log("player damage");
-
+            playerHP.GetDamaged(1);
         }
     }
 }
