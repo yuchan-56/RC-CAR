71ce658f15ca96f798d542b7061bcdf7e614cc9c
rlaeodbsdk <147009763+rlaeodbsdk@users.noreply.github.com>
2025-04-27 01:22:08 +0900

    오류사라진거 일단 푸시해보기 + 로딩신
    214

--- Assets/LoadingScene.cs
+++ Assets/LoadingScene.cs
@@ -7,6 +7,10 @@ using DG.Tweening;
 public class LoadingScene : MonoBehaviour
 {
     public Image LoadingPanel;
+    public Image running;
+    public Image loading;
+    public Sprite[] runSprites;
+    public Sprite[] loadingSprties;
     public static LoadingScene Instance
     {
         get
@@ -47,13 +51,54 @@ public class LoadingScene : MonoBehaviour
         LoadingPanel.DOFade(1f, 0.5f).SetUpdate(true);
 
         yield return new WaitForSecondsRealtime(0.5f);
+        setActive(true);
+
         Managers.Scene.LoadScene(scene);
 
-        yield return new WaitForSecondsRealtime(0.3f);
+
+        yield return new WaitForSecondsRealtime(2f);
         Debug.Log("이곳에 진입했습니다");
+
+        setActive(false);
+
         LoadingPanel.DOFade(0f, 0.3f).SetUpdate(true);
 
         Time.timeScale = 1;
         Managers.UI.ReSet();
     }
+
+    IEnumerator Animation_RunGo()
+    {
+        running.sprite = runSprites[5];
+        for (int i = 0; i < runSprites.Length; i++)
+        {
+            running.sprite = runSprites[i];
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+        StartCoroutine(Animation_RunGo());
+
+
+    }
+    IEnumerator Animation_LoadGo()
+    {
+        loading.sprite = loadingSprties[3];
+        for (int i = 0; i < loadingSprties.Length; i++)
+        {
+            loading.sprite = loadingSprties[i];
+            yield return new WaitForSecondsRealtime(0.1f);
+        }
+        StartCoroutine(Animation_LoadGo());
+    }
+    void setActive(bool tf)
+    {
+        running.gameObject.SetActive(tf);
+        loading.gameObject.SetActive(tf);
+        if(tf==true)
+        {
+            StartCoroutine(Animation_RunGo());
+            StartCoroutine(Animation_LoadGo());
+        }
+        else { StopAllCoroutines(); }
+    }
 }
+
