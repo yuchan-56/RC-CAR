10443ea1d14595af3e5d4e1fde3a117a0a3971da
choijeanwoo <jwoochoi0210@gmail.com>
2025-04-28 13:02:19 +0900

    터치 환경에서 작동하게끔 수정

--- Assets/Scripts/Managers/ComboManager.cs
+++ Assets/Scripts/Managers/ComboManager.cs
@@ -103,28 +103,37 @@ public class ComboManager : MonoBehaviour
             atkAni.isAnimating = false;
             dashAni.isAnimating = false;
             jumpAni.isAnimating = false;
+            InputButton.Clear();
+            AniSetup = false;
+            AniSetup2 = false;
         }
         else
         {
             //empty
         }
 
-        if (Input.GetMouseButton(0))
+        if (Input.touchCount > 0)
         {
-            if (EventSystem.current.IsPointerOverGameObject())
+            for (int i = 0; i < Input.touchCount; i++)
             {
-                PointerEventData pointerData = new PointerEventData(EventSystem.current);
-                pointerData.position = Input.mousePosition;
+                Touch touch = Input.GetTouch(i);
 
-                raycastResults.Clear();
-                EventSystem.current.RaycastAll(pointerData, raycastResults);
-
-                if (raycastResults.Count > 0)
+                if (touch.phase == TouchPhase.Began ||
+                    touch.phase == TouchPhase.Moved ||
+                    touch.phase == TouchPhase.Stationary)
                 {
-                    CurrentObject = raycastResults[0].gameObject;
-                }
+                    PointerEventData pointerData = new PointerEventData(EventSystem.current);
+                    pointerData.position = touch.position;
 
-                InputButton.Add(CurrentObject.name);
+                    raycastResults.Clear();
+                    EventSystem.current.RaycastAll(pointerData, raycastResults);
+
+                    if (raycastResults.Count > 0)
+                    {
+                        CurrentObject = raycastResults[0].gameObject;
+                        InputButton.Add(CurrentObject.name);
+                    }
+                }
             }
 
             if (InputButton.Count == 1 && AniSetup == false)
@@ -265,94 +274,117 @@ public class ComboManager : MonoBehaviour
             jumpAni.isAnimating = false;
         }
 
-        else if (Input.GetMouseButtonUp(0) && AniSetup == true)
+        if(InputButton.Count > 0)
         {
-
-            if (Managers.Game.isHit || player.buttonDeactive == true)
+            for (int i = 0; i < Input.touchCount; i++)
             {
-                return;
+                Touch touch = Input.GetTouch(i);
+
+                if (touch.phase == TouchPhase.Ended)
+                {
+                    PointerEventData pointerData = new PointerEventData(EventSystem.current);
+                    pointerData.position = touch.position;
+
+                    raycastResults.Clear();
+                    EventSystem.current.RaycastAll(pointerData, raycastResults);
+
+                    if (raycastResults.Count > 0)
+                    {
+                        GameObject releasedObject = raycastResults[0].gameObject;
+                        string releasedName = releasedObject.name;
+
+                        if (releasedName == "Attack" || releasedName == "Jump" || releasedName == "Dash")
+                        {
+                            ExecuteSkill();
+                        }
+                    }
+                }
             }
-
-            
-            playerAttackGeneral.AttackSetDeactive();
-            dashAttack.SkillMotionDeactive();
-            jumpAttack.SkillMotionDeactive();
-            StopExistingCoroutine(ref atkAniCoroutine);
-            StopExistingCoroutine(ref dashAniCoroutine);
-            StopExistingCoroutine(ref jumpAniCoroutine);
-            lgo.ImageDisabled();
-            rgo.ImageDisabled();
-            lugo.ImageDisabled();
-            ldgo.ImageDisabled();
-            rugo.ImageDisabled();
-            rdgo.ImageDisabled();
-            atkblink.ImageDisabled();
-            jumpblink.ImageDisabled();
-            dashblink.ImageDisabled();
-
-            atkAni.ResetImage();
-            dashAni.ResetImage();
-            jumpAni.ResetImage();
-            atkAni.isAnimating = false;
-            dashAni.isAnimating = false;
-            jumpAni.isAnimating = false;
-
-            if (player.IsAttacking)
-            {
-                InputButton.Clear();
-                return;
-            }
-
-            switch (InputButton.Count)
-            {
-                case 1:
-                    if (InputButton.Contains("Attack"))
-                    {
-                        
-                        playerAttackGeneral.AttackSetActive();
-                        player.SkillMotionActive("Attack");
-                    }
-                    break;
-
-                case 2:
-                    if (InputButton.Contains("Jump") && InputButton.Contains("Dash"))
-                    {
-
-                        
-                       
-                        player.SkillMotionActive("JumpDash");
-
-                    }
-                    else if (InputButton.Contains("Dash") && InputButton.Contains("Attack"))
-                    {
-                        
-                        
-                        dashAttack.SkillMotionActive();
-                        player.SkillMotionActive("DashAttack");
-                    }
-                    else if (InputButton.Contains("Jump") && InputButton.Contains("Attack"))
-                    {
-                        
-                    
-                        jumpAttack.SkillMotionActive();
-                        player.SkillMotionActive("JumpAttack");
-                    }
-                    break;
-
-                case 3:
-                    if (Managers.Game.gage >= 100 && InputButton.Contains("Jump") && InputButton.Contains("Attack") && InputButton.Contains("Dash")
-                        && playerAttackGeneral.UltimateSkill_Active == false)
-                    {
-                        
-                        playerAttackGeneral.UltimateSkillActive();
-                    }
-                    break;
-            }
-
-            AniSetup = false;
-            AniSetup2 = false;
-            InputButton.Clear();
         }
+    }
+
+    private void ExecuteSkill()
+    {
+        InputButton.Remove("Left");
+        InputButton.Remove("Right");
+
+        if (Managers.Game.isHit || player.buttonDeactive)
+        {
+            return;
+        }
+
+        playerAttackGeneral.AttackSetDeactive();
+        dashAttack.SkillMotionDeactive();
+        jumpAttack.SkillMotionDeactive();
+        StopExistingCoroutine(ref atkAniCoroutine);
+        StopExistingCoroutine(ref dashAniCoroutine);
+        StopExistingCoroutine(ref jumpAniCoroutine);
+
+        lgo.ImageDisabled();
+        rgo.ImageDisabled();
+        lugo.ImageDisabled();
+        ldgo.ImageDisabled();
+        rugo.ImageDisabled();
+        rdgo.ImageDisabled();
+        atkblink.ImageDisabled();
+        jumpblink.ImageDisabled();
+        dashblink.ImageDisabled();
+
+        atkAni.ResetImage();
+        dashAni.ResetImage();
+        jumpAni.ResetImage();
+        atkAni.isAnimating = false;
+        dashAni.isAnimating = false;
+        jumpAni.isAnimating = false;
+
+        if (player.IsAttacking)
+        {
+            InputButton.Clear();
+            return;
+        }
+
+        switch (InputButton.Count)
+        {
+            case 1:
+                if (InputButton.Contains("Attack"))
+                {
+                    playerAttackGeneral.AttackSetActive();
+                    player.SkillMotionActive("Attack");
+                }
+                break;
+
+            case 2:
+                if (InputButton.Contains("Jump") && InputButton.Contains("Dash"))
+                {
+                    player.SkillMotionActive("JumpDash");
+                }
+                else if (InputButton.Contains("Dash") && InputButton.Contains("Attack"))
+                {
+                    dashAttack.SkillMotionActive();
+                    player.SkillMotionActive("DashAttack");
+                }
+                else if (InputButton.Contains("Jump") && InputButton.Contains("Attack"))
+                {
+                    jumpAttack.SkillMotionActive();
+                    player.SkillMotionActive("JumpAttack");
+                }
+                break;
+
+            case 3:
+                if (Managers.Game.gage >= 100 &&
+                    InputButton.Contains("Jump") &&
+                    InputButton.Contains("Attack") &&
+                    InputButton.Contains("Dash") &&
+                    playerAttackGeneral.UltimateSkill_Active == false)
+                {
+                    playerAttackGeneral.UltimateSkillActive();
+                }
+                break;
+        }
+
+        AniSetup = false;
+        AniSetup2 = false;
+        InputButton.Clear();
     }
 
     private void StopExistingCoroutine(ref Coroutine coroutine)
